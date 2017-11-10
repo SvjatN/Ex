faf4666399f8adef801438e6ab8e901bd2f0ef71
SvjatN <svjatnab@gmail.com>
2017-11-10 19:58:48 +0200

    there are not sorting

--- 2/6.js
+++ 2/6.js
@@ -5,5 +5,6 @@ let c =1;
 let d =10;
 
 let arr =[a,b,c,d] ;
-  let arr2 = arr.map((item) => +item ).sort();
- console.log( arr2);
+  let arr2 = arr.map((item) => Number(item) );
+    a =  arr2.sort();
+ console.log(  a );
