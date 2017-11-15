9f9c3ee3e85a07904d702014319d5cf2c65342aa
SvjatN <svjatnab@gmail.com>
2017-11-16 00:49:38 +0200

    repair 2

--- 3/2.js
+++ 3/2.js
@@ -6,9 +6,10 @@ let sumOfTrirdNum = 0;
 for (let i = 0; i < countNum; i++) {
     let temp = Math.floor(Math.random() * 10000);
 
-    arr.push(temp);
-    if ( String(temp).length == 3) {
-        sumOfTrirdNum += 1;
+     arr.push(temp);
+    if ( String(arr[i]).length == 3) {
+
+        sumOfTrirdNum += arr[i] ;
     }
 }
 console.log(arr);
