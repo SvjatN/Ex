eb225cf8a0b570b494d41c84f667ec72beeb2550
SvjatN <svjatnab@gmail.com>
2017-11-17 01:10:12 +0200

    repair 5

--- 3/5.js
+++ 3/5.js
@@ -3,10 +3,10 @@ let arr = [];
 let countSecond = 0;
 
 for (let i = 0; i < N; i++) {
-    let temp = Math.round(Math.random() * 1000);
+    let temp = Math.round(Math.random() * 100);
     arr.push(temp);
 }
 
 console.log(N);
 console.log(arr.join(" "));
-console.log(arr.filter((item) => item.toString().split('').pop() == 2).join(' '));
+console.log(arr.filter((item) => item.toString().split('').pop() == 2).reduce((sum ,item)=>sum +=item ,0));
