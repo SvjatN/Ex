0dda803b13aea3978dc4bdac101584964bb5ae76
SvjatN <svjatnab@gmail.com>
2017-11-17 15:26:38 +0200

    make 6

---
+++ 3/6.js
@@ -0,0 +1,11 @@
+let N = 50;
+let arr = [];
+
+for (let i = 0; i < N; i++) {
+  arr.push(i);
+}
+
+console.log(N);
+console.log(arr.filter((item) => Math.sqrt(item) == Math.floor(Math.sqrt(item)))
+  .filter((item) => item > 0)
+  .join(" "));
