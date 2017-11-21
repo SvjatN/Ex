a74b14cb7dc2dc446c7fd8a0040840810532ced7
SvjatN <svjatnab@gmail.com>
2017-11-21 03:30:44 +0200

    make 15

---
+++ 3/15.js
@@ -0,0 +1,25 @@
+let n = 30000;
+let sum = 0;
+let arr = [];
+
+for(let i = 1 ;i<=n ;i++){
+    if(isPerfect(i) ){
+        sum ++;
+    }
+}
+
+function isPerfect(n){
+    let sum = 0;
+    for (let i = 1; i < n; i++) {
+        if (n % i == 0) {
+            sum += i;
+        }
+    }
+    if (n == sum) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(sum);
