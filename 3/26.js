84d14b2dbdb75c43bfdb81d9c159e807063576f0
SvjatN <svjatnab@gmail.com>
2017-12-07 01:34:27 +0200

    repair 26

---
+++ 3/26.js
@@ -0,0 +1,10 @@
+let arr = [3, 4, 9, 6, 2, 7, 8, 9];
+let max = - Infinity;
+
+for(let i = 0 ;i<arr.length;i++){
+    if( arr[i] > max ){
+        max = arr[i];
+    }
+}
+
+console.log(arr.indexOf(max) +1);
