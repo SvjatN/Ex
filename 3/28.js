8e7a0a6f7834da1da2cec162514c9af6ba51822e
SvjatN <svjatnab@gmail.com>
2017-12-18 00:15:29 +0200

    repair 28

--- 3/28.js
+++ 3/28.js
@@ -1,19 +1,23 @@
-let arr = [  7, 8, 1, 2, 2, 3, 1, 4, 1,1,1,1,1,1];
+let arr = [1, 0, 1, 0, 2, 0, 2, 0, 0];
 let temp = 2;
 let arrLength = [];
 
-for (let i = 1; i < arr.length ; i++) {
+for (let i = 1; i < arr.length -1; i++) {
+
     if ((arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) ||
         (arr[i] < arr[i - 1] && arr[i] < arr[i + 1])) {
         temp = temp + 1;
-        
+        if (i === arr.length-2) {
+            arrLength.push(temp);     
+        }
     } else {
         arrLength.push(temp);
         temp = 2;
-    } 
-    //console.log(arr[i - 1] + "  " + arr[i] + "  " + arr[i + 1]);
-    //console.log(temp);
-    //console.log((arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) ||
-    //(arr[i] < arr[i - 1] && arr[i] < arr[i + 1]));
+    }
+    console.log(arr[i - 1] + "  " + arr[i] + "  " + arr[i + 1]);
+    console.log(temp);
+    console.log((arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) ||
+        (arr[i] < arr[i - 1] && arr[i] < arr[i + 1]));
 }
+console.log(arrLength);
 console.log(Math.max(...arrLength));
