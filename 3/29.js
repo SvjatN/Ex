acc996fdb45ef43b679397b6bace46a3069461c5
Nabi <svjatnab@gmail.com>
2017-12-30 04:13:16 +0200

    try from linux

--- 3/29.js
+++ 3/29.js
@@ -1,12 +1,35 @@
-let arr = [1,1,1,1,1,1,123, 12, 45, 3, 5, 6, 7, 1, 2];
+let arr = [1, 2, 3, 4, 5, 4, 3, 2, 2, 1, 3, 4];
 let temp = 0;
-let arrTemp = [];
+let temp2 = 0;
+let arrTemp1 = [];
+let arrTemp2 = [];
+
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i] <=arr[i + 1]) {
+    if (arr[i] <= arr[i + 1]) {
         temp = temp + 1;
+        if (i === arr.length-2) {
+            arrTemp1.push(temp+1);     
+        }
     } else {
-        arrTemp.push(temp + 1);
+        arrTemp1.push(temp + 1);
         temp = 0;
     }
+    console.log(+ arr[i] + "  " + arr[i + 1]);
+    console.log(temp);
 }
-console.log(Math.max(...arrTemp));
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= arr[i + 1]) {
+        temp2 = temp2 + 1;
+        if (i === arr.length-2) {
+            arrTemp2.push(temp2+1);     
+        }
+    } else {
+        arrTemp2.push(temp2 + 1)
+        temp2 = 0;
+    }
+    console.log(+ arr[i] + "  " + arr[i + 1]);
+    console.log(temp2);
+}
+console.log(arrTemp1);
+console.log(arrTemp2);
+console.log(Math.max(...arrTemp1, ...arrTemp2));
